8ad54a717f70f0cf35ba436052ff08b78ba8e727
q1p1 <meshari.algahbban@wakecap.com>
2024-12-11 14:20:33 +0300

    test contains

--- src/components/Add3DPoints.ts
+++ src/components/Add3DPoints.ts
@@ -17,7 +17,7 @@ export const add3DPoints = (graphicsLayer: GraphicsLayer): Graphic[] => {
         <ul>
           <li><b>Age:</b> ${point.age}</li>
           <li><b>Company:</b> ${point.company}</li>
-          <li><b>Job ID:</b> ${point.jobId}</li>
+          <li><b>Job ID:</b> ${point.jobId}</li> 
         </ul>
       `,
     });
